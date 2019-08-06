246c3a2b8ac823784a616165d3ecc3ce974f6f70
ivanbeshka <ivanbeshka2002@gmail.com>
2019-08-06 14:39:05 +0500

    Дз №4

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,34 +13,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText1;
-    private EditText editText2;
-    private Button btnTo2nd;
-
-    private String strKey = "str";
-    private String intKey = "int";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText1 = findViewById(R.id.et_top);
-        editText2 = findViewById(R.id.et_bottom);
-        btnTo2nd = findViewById(R.id.btn_to_second_activity);
-
-        final Intent intent = new Intent(this, SecondActivity.class);
-
-        btnTo2nd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text1 = String.valueOf(editText1.getText());
-                Integer int2 = Integer.valueOf(String.valueOf(editText2.getText()));
-                intent.putExtra(strKey,text1);
-                intent.putExtra(intKey,int2);
-
-                startActivity(intent);
-            }
-        });
     }
 }
